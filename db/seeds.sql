4abc2aebc0feb149470b154476b03f72d518b67d
amaher <amaher112@su.edu>
2024-02-12 22:01:08 -0500

    added sql files, created schema and inquirer prompts

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO department (name)
+VALUES ('Marketing'),
+        ('Legal'),
+        ('Finance'),
+        ('Engineering');
+
+INSERT INTO role (title, salary)
+VALUES ('Accountant', 120000),
+        ('Software Engineer', 130000),
+        ('Lawyer', 250000),
+        ('Salesperson', 90000),
+        ('Account Manager', 100000),
+        ('Legal Aide', 80000),
+        ('Lead Engineer', 150000);
+
+INSERT INTO employee (first_name, last_name)
--- server.js
+++ server.js
@@ -1,4 +1,39 @@
-const mysql = require('mysql2');
+const express = require("express");
+const mysql = require("mysql2");
 const inquirer = require("inquirer");
-require('dotenv').config();
+require("dotenv").config();
 
+// Middleware
+const app = express();
+const PORT = 3001;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: process.env.DBUSER,
+      password: process.env.DBPASSWORD,
+      database: process.env.DB_NAME
+    },
+    console.log(`Connected to the employee_tracker database.`)
+  );
+
+inquirer.prompt([
+  {
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+      "View all departments",
+      "View all roles",
+      "View all employees",
+      "Add a department",
+      "Add a role",
+      "Add an employee",
+      "Update an employee role",
+    ],
+    name: "?",
+  },
+]);
